f207c997511383335747accda1fe14ae2d8223e6
Lexa622 <lexa622@gmail.com>
2024-08-25 23:27:16 +0300

    Домашнее задание по теме "Интроспекция"

--- Интроспекция.py
+++ Интроспекция.py
@@ -1,7 +1,8 @@
 def introspection_info(obj):
     obj_type = type(obj).__name__
-    attributes = dir(obj)
-    methods = [attribute for attribute in attributes if callable(getattr(obj, attribute))]
+    dir_ = dir(obj)
+    attributes = [attribute for attribute in dir_ if not callable(getattr(obj, attribute))]
+    methods = [method for method in dir_ if callable(getattr(obj, method))]
     module = obj.__class__.__module__
     info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module}
     return info
